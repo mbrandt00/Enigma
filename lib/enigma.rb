21c0e145f6571fe1b025787d16f0b042dc4d97f4
Michael Brandt <msbrandt00@gmail.com>
2022-01-08 18:22:45 -0700

    clean up #generate_key to allow for leading 0s, #todays_date simplified

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,17 +1,21 @@
 require 'date'
+require 'pry'
 class Enigma
+  attr_reader :today
   def initialize
   end
 
-  def encrypt(message,key=random_key,date=todays_date)
+  def encrypt(message, key = self.generate_key, date = self.todays_date)
+    # find_shifters(key)
     #return {
     # :encryption => encrypted_string,
     # :key => key usedfor encryption as string
     # :date => date used for encryption as string in DDMMYY
   # }
+  p message, key, date
   end
 
-  def encrypt(ciphertext, key, date)
+  def unencrypt(ciphertext, key, date)
     # returns {
     # :decryption => decrypted String
     # :key => key used for encryption as string
@@ -19,4 +23,15 @@ class Enigma
   # }
   end
 
+  def generate_key
+    random_number = rand(0..99999)
+    return random_number.to_s.rjust(5,'0')
+  end
+
+  def todays_date
+    today = Date.today.strftime('%m%d%y')
+  end
+
 end
+a = Enigma.new
+p a.generate_key
